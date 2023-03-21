bb9ac812f91acdfb54297d1478bdf3e09b575140
CryptoSailors <111496704+CryptoSailors@users.noreply.github.com>
2023-03-21 12:44:59 +0200

    Create ethereum_update.sh

---
+++ Testnets/ETH-Goerli-RPC/ethereum_update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -e '\n\e[42mUpgrade Ethereum Node\e[0m\n' && sleep 1
+source .bash_profile
+cd go-ethereum
+git pull
+latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
+git checkout $latestTag
+make
+
+echo -e '\n\e[42mCheck Ethereum version\e[0m\n' && sleep 1
+sudo mv build/bin/geth /usr/bin/
+geth version && slep 3
+sudo systemctl restart geth 
+sudo journalctl -u geth -f -n 100
