5e5e18c9d0ca299112e65cdff1f99340b83ca9fc
CryptoSailors <111496704+CryptoSailors@users.noreply.github.com>
2023-03-27 13:28:51 +0300

    Create lighthouse_update.sh

---
+++ Testnets/ETH-Goerli-RPC/lighthouse_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e '\n\e[42mUpgrade lighthouse Beacone\e[0m\n' && sleep 1
+source .bash_profile
+cd lighthouse
+git pull
+latestTag=$(curl -s https://api.github.com/repos/sigp/lighthouse/releases/latest | grep '.tag_name'|cut -d\" -f4)
+echo $latestTag
+git checkout $latestTag
+make
+
+echo -e '\n\e[42mCheck lighthouse version and start a beacone\e[0m\n' && sleep 1
+cd ~
+./.cargo/bin/lighthouse --version
+sudo systemctl restart lighthouse 
+sudo journalctl -u lighthouse -f -n 100
